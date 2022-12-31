810044f4ffa9b0a2dc6397e9ba54aa677deebdc1
Vahid Porki <ivahidp@gamil.com>
2022-12-31 12:13:11 +0330

    Use dispatch to decrement and increment counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,9 +1,17 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import classes from "./Counter.module.css";
 
 const Counter = () => {
+	const dispatch = useDispatch();
 	const counter = useSelector((state) => state.counter);
+
+	const incrementHandler = () => {
+		dispatch({ type: "increment" });
+	};
+	const decrementHandler = () => {
+		dispatch({ type: "decrement" });
+	};
 
 	const toggleCounterHandler = () => {};
 
@@ -13,6 +21,8 @@ const Counter = () => {
 		<main className={classes.counter}>
 			<h1>Redux Counter</h1>
 			<div className={classes.value}>{counter}</div>
+			<button onClick={incrementHandler}>Increment</button>
+			<button onClick={decrementHandler}>Decrement</button>
 			<button onClick={toggleCounterHandler}>Toggle Counter</button>
 		</main>
 	);
--- src/components/store/index.js
+++ src/components/store/index.js
@@ -1,19 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-function counterReducer(state = { counter: 0 }, action) {
+function counterReducer(state = { counter: 1 }, action) {
 	// Check to see if the reducer cares about this action
 	if (action.type === "increment") {
 		// If so, make a copy of `state`
 		return {
 			...state,
 			// and update the copy with the new value
-			value: state.value + 1,
+			counter: state.counter + 1,
 		};
 	}
 	if (action.type === "decrement") {
 		return {
 			...state,
-			value: state.value - 1,
+			counter: state.counter - 1,
 		};
 	}
 	// otherwise return the existing state unchanged
