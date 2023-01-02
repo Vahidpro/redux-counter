d843c389455c3144966c8afefb5913b065467737
Vahid Porki <ivahidp@gamil.com>
2023-01-02 12:23:31 +0330

    Added login form

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,37 @@
-import classes from './Header.module.css';
+import { useSelector, useDispatch } from "react-redux";
+
+import classes from "./Header.module.css";
+import { authActions } from "../store/auth";
 
 const Header = () => {
-  return (
-    <header className={classes.header}>
-      <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+	const dispatch = useDispatch();
+	const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
+	const logoutHandler = () => {
+		dispatch(authActions.logout());
+		console.log("logout!");
+	};
+
+	return (
+		<header className={classes.header}>
+			<h1>Redux Auth</h1>
+			{isAuth && (
+				<nav>
+					<ul>
+						<li>
+							<a href="/">Products</a>
+						</li>
+						<li>
+							<a href="/">Sales</a>
+						</li>
+						<li>
+							<button onClick={logoutHandler}>Logout</button>
+						</li>
+					</ul>
+				</nav>
+			)}
+		</header>
+	);
 };
 
 export default Header;
